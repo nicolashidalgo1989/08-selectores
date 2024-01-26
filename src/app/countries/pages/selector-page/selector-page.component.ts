f90984a3017d429f5f2c4e7057409a9f7efa1edb
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2024-01-25 22:22:15 -0300

    fix: clear country select value

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountryService } from '../../services/country.service';
 import { Country, Region, SmallCountry } from '../../interfaces/country.inteface';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   templateUrl: './selector-page.component.html'
@@ -35,6 +35,7 @@ export class SelectorPageComponent implements OnInit {
   onRegionChanged(): void {
     this.myForm.get('region')!.valueChanges
       .pipe(
+        tap( () => this.myForm.get('country')!.setValue('') ),
         switchMap( region => this.cs.getCountriesByRegion(region) ),
       )
       .subscribe( countries => {
